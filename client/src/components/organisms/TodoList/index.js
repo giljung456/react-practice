cca7be101b5d6662f40957480dd160339ce1d324
giljung456 <uhwan0723@gmail.com>
2021-10-10 07:05:55 +0900

    feat: todoItem 컴포넌트에서 체크 버튼 / 삭제 버튼을 눌렀을 때 각각 수정 / 삭제 api를 호출하는 기능 추가

--- client/src/components/organisms/TodoList/index.js
+++ client/src/components/organisms/TodoList/index.js
@@ -1,21 +1,28 @@
 import { useState, useEffect } from "react";
 import { TodoItem } from "../../molecules";
-import getTodos from "../../../apis/getTodos";
+import { getUserTodos } from "../../../apis/user";
 import "./index.scss";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
   useEffect(() => {
     const fetchTodos = async () => {
-      const todos = await getTodos();
+      const todos = await getUserTodos();
       setTodos(todos);
     };
 
     fetchTodos();
   }, []);
+
+  const onItemChange = (todoId) => {
+    const newTodos = todos.filter((todo) => todo.id !== todoId);
+    setTodos(newTodos);
+  };
+
   const todoList = todos.map((todo) => {
-    return <TodoItem title={todo.content} key={String(todo.id)}></TodoItem>;
+    return <TodoItem id={todo.id} onItemChange={onItemChange} content={todo.content} key={String(todo.id)}></TodoItem>;
   });
+
   return <div className="todo-list">{todoList}</div>;
 }
 
