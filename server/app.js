c4ad6c1cf7c64a86ed6fddd75f1f0943a3c878f6
giljung456 <uhwan0723@gmail.com>
2021-10-10 05:36:48 +0900

    feat: dummy 데이터 및 todolist를 get해오는 임시 api 추가

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import cors from "cors";
+import { todos } from "./models/dummy.js";
 
 const __dirname = path.resolve();
 const app = express();
@@ -14,5 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/users/my/todos", (req, res) => res.status(200).json(todos));
 
 app.listen(port);
---
+++ server/models/dummy.js
@@ -0,0 +1,26 @@
+export const todos = [
+  {
+    content: "테스트 0",
+    id: 0,
+  },
+  {
+    content: "테스트 1",
+    id: 1,
+  },
+  {
+    content: "테스트 2",
+    id: 2,
+  },
+  {
+    content: "테스트 3",
+    id: 3,
+  },
+  {
+    content: "테스트 4",
+    id: 4,
+  },
+  {
+    content: "테스트 5",
+    id: 5,
+  },
+];
