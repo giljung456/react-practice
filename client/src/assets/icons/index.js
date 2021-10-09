b53cfd2738fb904fe2d7ef9ba6961e3271cfd77c
giljung456 <uhwan0723@gmail.com>
2021-10-10 01:55:37 +0900

    chore: 필요한 icon 다운로드

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,5 @@
+export { default as add } from "./add.png";
+export { default as chat } from "./message.png";
+export { default as user } from "./user.png";
+export { default as check } from "./check.png";
+export { default as close } from "./close.png";
