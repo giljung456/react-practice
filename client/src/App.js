b670f602e068ec6fb806da95ebd27523bba50d6c
giljung456 <uhwan0723@gmail.com>
2021-10-10 19:00:04 +0900

    update: github 인증으로 회원가입 하는 방식 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Main, Start, Auth } from "./pages";
+import { Main, Start, Auth, Register } from "./pages";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import route from "./commons/constants/route";
 
@@ -9,6 +9,7 @@ function App() {
         <Route path={route.MAIN} component={Main} exact></Route>
         <Route path={route.START} component={Start}></Route>
         <Route path={route.AUTH} component={Auth}></Route>
+        <Route path={route.REGISTER} component={Register}></Route>
       </Router>
     </div>
   );
